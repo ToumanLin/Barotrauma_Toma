c676233dfd4ecab0a013958b2cf6740640fa67c7
MapleWheels <njainanan@hotmail.com>
2026-02-27 17:56:47 -0500

    [Save/Sync] Work on SettingsMenu

--- Barotrauma/BarotraumaClient/ClientSource/LuaCs/Services/ModsGameplaySettingsMenu.cs
+++ Barotrauma/BarotraumaClient/ClientSource/LuaCs/Services/ModsGameplaySettingsMenu.cs
@@ -28,22 +28,41 @@ internal sealed class ModsGameplaySettingsMenu : ModsSettingsMenu
             new RectTransform(new Vector2(0.85f, 0.1f), searchBarLayoutGroup.RectTransform, Anchor.TopLeft),
             createClearButton: true)
         {
-            OnEnterPressed = (btn, txt) =>
+            OnTextChangedDelegate = (btn, txt) =>
             {
+                // TODO: Execute filter here
                 return true;
             }
         };
-        // display area
+        // main display area
         var settingsContentAreaGroup = new GUILayoutGroup(new RectTransform(new Vector2(1f, 0.90f), contentAreaLayoutGroup.RectTransform, Anchor.BottomCenter));
         GUIUtil.Spacer(settingsContentAreaGroup, Vector2.One);
         var (modCategoryDisplayGroup, settingsDisplayGroup) = GUIUtil.CreateSidebars(settingsContentAreaGroup, true);
         modCategoryDisplayGroup.RectTransform.RelativeSize = new Vector2(0.3f, 1f);
         settingsDisplayGroup.RectTransform.RelativeSize = new Vector2(0.7f, 1f);
+
+        // Mods & Category Selectors
         var cpList = packageManagementService.GetAllLoadedPackages().OrderBy(cp => cp.Name == "Vanilla" ? 0 : 1).ThenBy(cp => cp.Name).ToList();
         var modSelectDropDown = GUIUtil.Dropdown<ContentPackage>(modCategoryDisplayGroup, cp => cp.Name == "Vanilla" ? "All" : cp.Name, null, cpList, cpList[0], cp =>
         {
-            // filter selections
+            // TODO: filter selections by adding it to the search bar
         }, Vector2.One, 2);
+
+
+        void GenerateDisplayFromFilter(string text)
+        {
+
+        }
+
+        void GenerateCategoryListDisplay(GUILayoutGroup layoutGroup)
+        {
+
+        }
+
+        void GenerateSettingsListDisplay(GUILayoutGroup layoutGroup) 
+        {
+
+        }
     }
 
 
